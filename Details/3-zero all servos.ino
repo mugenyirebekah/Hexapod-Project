97402e8a43aad6cc7174c9bc12edc8737ed19a3f
Rebekah Mugenyi <rebekahmish16@gmail.com>
2025-02-04 04:06:55 +0300

    Create 3-zero all servos.ino

---
+++ Details/3-zero all servos.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+
+Servo servos[18];
+
+void setup() {
+  Serial.begin(9600);
+  for (int i = 0; i < 18; i++) {
+    servos[i].attach(i + 22); // Start from pin 22
+    servos[i].write(90); // Set to middle position
+  }
+}
+
+void loop() {}
